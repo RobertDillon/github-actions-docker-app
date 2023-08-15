61491ccd04052bbb460dbf80c5580ff2d62f2bf4
Robert Dillon <robdillon94@gmail.com>
2023-08-15 23:54:30 +0200

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
-COPY target/docker-github-actions-demo-app1-0.0.2-SNAPSHOT.jar app.jar
+COPY target/docker-github-actions-demo-app1-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/dev/robdillon/dockergithubactionsdemoapp1/scheduler/LogMessageScheduler.java
+++ src/main/java/dev/robdillon/dockergithubactionsdemoapp1/scheduler/LogMessageScheduler.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 public class LogMessageScheduler {
 
-    
     @Scheduled(fixedRate = 30000)
     public void printMessage() {
-        System.out.println("Hello from Docker Demo App 1! This is version 0.0.2");
+        System.out.println("Hello from Docker Demo App 1! This is version 0.0.1");
     }
 
 }
